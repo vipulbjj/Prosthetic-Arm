ee2eeb90d38af6cfef73c71793ae32a3b130a578
shivankgarg98 <shivankgarg98@gmail.com>
2017-07-08 20:09:23 +0530

    finale

---
+++ master.ino
@@ -0,0 +1,35 @@
+int a[4];
+int thd_0=490;
+int thd_1=496;//fuckfin
+int thd_2=505;//forefin
+int thd_3=503;//Mthumb
+int thd_4=511;
+int val=0;
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {//Serial.println(analogRead(A0));//delay(2000);Serial.println(analogRead(A1));delay(2000);Serial.println(analogRead(A2));delay(2000);
+  val=0;
+if(analogRead(A0)>thd_0)
+  a[0]=1;
+else a[0]=0;
+if(analogRead(A1)>thd_1)
+  a[1]=1;
+else a[1]=0;
+if(analogRead(A2)>thd_2)
+  a[2]=1;
+else a[2]=0;
+if(analogRead(A3)>thd_3)
+  a[3]=1;
+else a[3]=0;
+//if(analogRead(A4)>thd_4)
+//  a[4]=1;
+//else a[4]=0;
+for(int i=0;i<4;i++)
+{
+  val+=pow(2,i)*a[i];
+}
+Serial.write(val);
+delay(200);
+}
---
+++ slave.ino
@@ -0,0 +1,53 @@
+#include<Servo.h>
+Servo servo_0;
+Servo servo_1;
+Servo servo_2;
+Servo servo_3;
+//Servo servo_4;
+int pos=0;
+int arr[2];
+//int servo_pos=0;
+void setup() {
+  // put your setup code here, to run once:
+//pinMode(13,OUTPUT);
+//pinMode(12,OUTPUT);
+servo_0.attach(8);
+servo_1.attach(9);
+servo_2.attach(10);
+servo_3.attach(11);
+//servo_4.attach(12);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available()>0){
+  pos=Serial.read();
+  for(int i=0;i<5;i++)
+  {
+    arr[i]=pos%2;
+    pos=pos/2;
+  }
+    if(arr[0]==1)
+     servo_0.write(180);
+    else
+    servo_0.write(0);
+   if(arr[1]==1)
+     servo_1.write(180);
+    else 
+    servo_1.write(0);
+    if(arr[2]==1)
+     servo_2.write(180);
+    else
+    servo_2.write(0);
+    if(arr[3]==1)
+     servo_3.write(180);
+    else
+    servo_3.write(0);
+  //  if(arr[4]==1)
+    // servo_4.write(180);
+    //else 
+    //servo_4.write(0); 
+}
+delay(100);
+
